f4e9c2e0cccd80f61e4dd03d59f874df9e7d2a97
EleazarSauz <eleazar48a@gmail.com>
2020-07-02 23:17:04 -0500

    home & about navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,20 @@
 import Head from 'next/head'
 import '../styles.css'
+import NavBar from '../components/NavBar.jsx'
 
 
-
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
     return (
     <>
         <Head>
-        <title>PairHub</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
+            <title>PairHub</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
         </Head>
 
+        <NavBar/>
         <Component {...pageProps} />
     </>
     )
---
+++ resources/functions.js
@@ -0,0 +1,17 @@
+const TOKEN = 'ukoSxdM_qP_zKI2Xe5VBgM43tK_UEGMH7AZvPntY1Vk'
+const URI = 'https://api.unsplash.com/photos/?client_id='
+
+export const getListImg = async () => {
+    try {
+        var getData = await fetch(URI + TOKEN)
+        var parseData = await getData.json()
+        if (getData.status === 400) {
+            throw parseData;
+        }
+    
+        return parseData
+    } catch (error) {
+        console.error('getListRepoApi', error)
+        throw error;
+    }
+}
